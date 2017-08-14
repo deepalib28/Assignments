3f5bda3c2d857f116e06835abe74a241df40d35a
deepali <dbhavsar@tavisca.com>
2017-08-14 13:38:46 +0530

    Override toString method in a class

---
+++ OverrideToString/OverrideToString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud=new Student("Deepali","Bhavsar");
+
+            string fullName=stud.ToString();
+
+            Console.WriteLine("fullName of student is:   "+fullName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OverrideToString/OverrideToStringTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OverrideToString;
+namespace OverrideToStringTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsToString_Sucessfull()
+        {
+            Student stud=new Student("Dee","Bhavsar");
+            string fullName=stud.ToString();
+            Assert.AreEqual(fullName, "Dee Bhavsar");
+        }
+
+
+        [TestMethod]
+        public void IsToString_UnSucessfull()
+        {
+            Student stud = new Student("Dee", "Bhavsar");
+            string fullName = stud.ToString();
+            Assert.AreNotEqual(fullName, "DeeBhavsar");
+        }
+    }
+}
