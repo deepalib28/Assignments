aade0f5d016d73a6d92c60c5b0b7dc73157a2130
deepali <dbhavsar@tavisca.com>
2017-08-18 12:13:51 +0530

    Modified cache impl

--- CachingImplementation/CachingImplementation/Cache.cs
+++ CachingImplementation/CachingImplementation/Cache.cs
@@ -7,41 +7,47 @@ using System.Threading.Tasks;
 namespace CachingImplementation
 {
    
-    public class Cache
+    public class Cache:ICache
     {
-        public int count
-        {
-            get { return _cache.Count; }
-        }
+       
         private Dictionary<string, WeakReference> _cache { get; set; }
 
         public Cache()
         {
             _cache = new Dictionary<string, WeakReference>();
         }
-
-
-
+        
         public void Add(string key,Object o)
         {
             if(string.IsNullOrEmpty(key)==false && o!=null&&_cache.ContainsKey(key)==false)
             _cache.Add(key, new WeakReference(o, false));
         }
 
-        public Object Retrieve(string key)
+        public Object Get(string key)
         {
-            return _cache[key];
+           
+            if(string.IsNullOrEmpty(key)==false && _cache.ContainsKey(key))
+            return _cache[key].Target;
+
+            return new Exception("Key not Found exception");
+
         }
 
-        public Object Update(string id,Object o)
+        public Object Update(string key,Object o)
         {
-            _cache[id] = new WeakReference(o, false);
+            if (string.IsNullOrEmpty(key) == false && _cache.ContainsKey(key))
+                _cache[key] = new WeakReference(o, false);
+            else
+                _cache.Add(key, new WeakReference(o, false));
+
             return _cache;
         }
 
         public void Remove(string key)
         {
-            _cache.Remove(key);
+            if(string.IsNullOrEmpty(key) == false && _cache.ContainsKey(key))
+                _cache.Remove(key);
+
         }
 
         public void Clear()
---
+++ CachingImplementation/CachingImplementation/ICache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CachingImplementation
+{
+    interface ICache
+    {
+        void Add(string key, Object o);
+
+        Object Get(string key);
+
+        Object Update(string id, Object o);
+
+        void Remove(string key);
+
+        void Clear();
+    }
+}
