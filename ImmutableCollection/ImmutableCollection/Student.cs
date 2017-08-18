e00b3649b81657127c262f606230bad6b0dcd707
deepali <dbhavsar@tavisca.com>
2017-08-18 14:21:39 +0530

    Immutable collection implemenation

--- ImmutableCollection/ImmutableCollection/Student.cs
+++ ImmutableCollection/ImmutableCollection/Student.cs
@@ -8,6 +8,29 @@ namespace ImmutableCollection
 {
     public class Student
     {
-    
+        private readonly string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+        }
+
+        private readonly string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+          
+        }
+
+        public Student(string firstName,string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+        public string Concat(string firstName,string lastName)
+        {
+            return firstName + lastName;
+        }
     }
 }
